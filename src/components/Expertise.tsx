2416dc871de0b41daf33169bb6b2774c616197fa
Upreti Hardik (PSS PCS RD WCS PRE CE) <Hardik.Upreti@infineon.com>
2025-08-22 16:44:04 +0530

    updated components

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,44 +1,36 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faReact, faDocker, faPython, faMicrochip, faRobot } from '@fortawesome/free-brands-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
 const labelsFirst = [
-    "React",
-    "TypeScript",
-    "JavaScript",
-    "HTML5",
-    "CSS3",
-    "SASS",
-    "Flask",
-    "Python",
-    "SQL",
-    "PostgreSQL",
-    "Postman"
+    "LVDS RX",
+    "PLL",
+    "Voltage Regulator",
+    "PWM",
+    "DAC",
+    "Error Amplifier"
 ];
 
 const labelsSecond = [
-    "Git",
-    "GitHub Actions",
-    "Docker",
-    "AWS",
-    "Azure",
-    "Linux",
-    "Snowflake",
-    "Pandas",
-    "Selenium",
+    "Tektronix MSO54 Oscilloscope",
+    "Tektronix AFG 330000 Series",
+    "Keysight 81134A Pattern Generator",
+    "Keithley SMUs",
+    "Agilent Power Supplies",
+    "Cytec RF Switch",
+    "Multimeter",
+    "Keysight Spectrum Analyzer"
+
 ];
 
 const labelsThird = [
-    "OpenAI",
-    "Groq",
-    "LangChain",
-    "Qdrant",
-    "Hugging Face",
-    "LlamaIndex",
-    "Streamlit",
+    "PyVisa",
+    "Numpy",
+    "Pandas",
+    "Matplotlib"
 ];
 
 function Expertise() {
@@ -48,11 +40,11 @@ function Expertise() {
             <h1>Expertise</h1>
             <div className="skills-grid">
                 <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
+                    <FontAwesomeIcon icon={faMicrochip} size="3x"/>
                     <h3>Electrical Validation</h3>
                     <p>I have characterized various Analog IPs like LVDS RX, PLL, Voltage Regulator, Error Amplifier etc.</p>
                     <div className="flex-chips">
-                        <span className="chip-title">Tech stack:</span>
+                        <span className="chip-title">IPs Characterized:</span>
                         {labelsFirst.map((label, index) => (
                             <Chip key={index} className='chip' label={label} />
                         ))}
@@ -60,11 +52,11 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
+                    <FontAwesomeIcon icon={faRobot} size="3x"/>
                     <h3>Bench Setup Automation</h3>
                     <p>Once the Silicon Bringup is completed, I develop Python based automation using PyVisa library to enable data collection on the circuits with minimal manual intervention.</p>
                     <div className="flex-chips">
-                        <span className="chip-title">Tech stack:</span>
+                        <span className="chip-title">Instruments:</span>
                         {labelsSecond.map((label, index) => (
                             <Chip key={index} className='chip' label={label} />
                         ))}
@@ -76,7 +68,7 @@ function Expertise() {
                     <h3>Data Analysis and Visualization</h3>
                     <p>I have experience deveoping data analysis and visualization scripts to support data driven engineering decisions. </p>
                     <div className="flex-chips">
-                        <span className="chip-title">Tech stack:</span>
+                        <span className="chip-title">Libraries:</span>
                         {labelsThird.map((label, index) => (
                             <Chip key={index} className='chip' label={label} />
                         ))}
